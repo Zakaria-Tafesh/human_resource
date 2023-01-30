cd5ce1c44a2ea2d2c3b7d084dcab4b278fec9017
Zakaria <zakaria.tafesh@gmail.com>
2023-01-30 22:20:51 +0200

    client scripts

--- human_resource/human_resource/doctype/employee/employee.js
+++ human_resource/human_resource/doctype/employee/employee.js
@@ -6,3 +6,23 @@
 
 // 	},
 // });
+
+// employee - validate date_of_birth
+frappe.ui.form.on('Employee', {
+ validate(frm) {
+if (frm.doc.date_of_birth >= get_today()  ) {
+ frappe.throw('You can not select date of birth after today');
+}
+} })
+
+
+// employee - validate count_employee_education
+frappe.ui.form.on('Employee',  { validate: function(frm) {
+ var total_education = 0;
+ $.each(frm.doc.education,  function(i,  d) {
+ if ( d.school_university){
+ total_education += 1; }
+ });
+frm.doc.count_employee_education = total_education;
+}
+ });
